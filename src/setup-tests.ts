54d00de3811c57bc77f2353bdd7ffbdbbe61ecbb
Ardit Nurcaj <arditnurcaj@gmail.com>
2023-01-01 23:00:56 +0100

    move msw helpers in test folder

--- src/setup-tests.ts
+++ src/setup-tests.ts
@@ -2,9 +2,9 @@ import { cleanup } from '@testing-library/react';
 
 import '@testing-library/jest-dom/extend-expect';
 
-import { server } from '@/__mocks__/server';
+import { server } from '@/test/server';
 
-// Establish API mocking before all tests.
+// establish API mocking before all tests.
 beforeAll(() => server.listen());
 
 // runs a cleanup after each test case (e.g. clearing jsdom)
@@ -13,5 +13,5 @@ afterEach(() => {
   cleanup();
 });
 
-// Clean up after the tests are finished.
+// clean up after the tests are finished.
 afterAll(() => server.close());
