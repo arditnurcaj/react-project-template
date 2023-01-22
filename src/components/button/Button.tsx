90aa632ae69ce684e9dba50f17e6b6a65f0ba6bb
Ardit Nurcaj <arditnurcaj@gmail.com>
2023-01-22 20:03:41 +0100

    add components, features folders

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,5 @@
+const Button = (): JSX.Element => {
+  return <button>This is a button!</button>;
+};
+
+export default Button;
---
+++ src/features/index.ts
@@ -0,0 +1,2 @@
+// here come all features/pages which may contain other components, utils etc. specific to that feature
+export {};
